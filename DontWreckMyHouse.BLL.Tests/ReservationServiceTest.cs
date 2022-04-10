226ba6971a1574a6c9996b105b78c0ab67bd4db7
Mabel Quek <mxquek1@gmail.com>
2022-04-10 15:49:08 -0500

    Copied over RRepo to RRepoDouble

--- DontWreckMyHouse.BLL.Tests/ReservationServiceTest.cs
+++ DontWreckMyHouse.BLL.Tests/ReservationServiceTest.cs
@@ -1,7 +1,12 @@
 ﻿
+using DontWreckMyHouse.BLL.Tests.TestDoubles;
+
 namespace DontWreckMyHouse.BLL.Tests
 {
     public class ReservationServiceTest
     {
+        ReservationService reservationService = new ReservationService(new ReservationRepositoryDouble());
+
+        
     }
 }
--- DontWreckMyHouse.BLL.Tests/TestDoubles/ReservationRepositoryDouble.cs
+++ DontWreckMyHouse.BLL.Tests/TestDoubles/ReservationRepositoryDouble.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using DontWreckMyHouse.Core.Interfaces;
 using DontWreckMyHouse.Core.Models;
 
@@ -20,6 +21,8 @@ namespace DontWreckMyHouse.BLL.Tests.TestDoubles
 
         string Seed_Path = Path.Combine(DATA_DIRECTORY, SEED_DIRECTORY, SEED_DATA_DIRECTORY);
         string Test_Path = Path.Combine(DATA_DIRECTORY, TEST_DIRECTORY, TEST_DATA_DIRECTORY);
+
+        private const string HEADER = "id,start_date,end_date,guest_id,total";
 
         public ReservationRepositoryDouble()
         {
@@ -41,24 +44,179 @@ namespace DontWreckMyHouse.BLL.Tests.TestDoubles
             }
         }
 
-        public void Add(Result<Reservation> reservation, string hostID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Edit(Result<Reservation> updatedReservation, string hostID)
-        {
-            throw new NotImplementedException();
-        }
-
         public void GetReservationsByHostID(string HostID, Result<List<Reservation>> result)
         {
-            throw new NotImplementedException();
+            string filePath = GetFilePath(HostID);
+
+            if (!File.Exists(filePath))
+            {
+                result.Success = false;
+                result.Message = "Host file does not exist.";
+            }
+
+            else
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        string currentLine = sr.ReadLine();
+                        if (currentLine != null)
+                        {
+                            currentLine = sr.ReadLine();
+                        }
+                        while (currentLine != null)
+                        {
+                            Reservation reservation = Deserialize(currentLine.Trim());
+                            result.Data.Add(reservation);
+                            currentLine = sr.ReadLine();
+                        }
+                        result.Success = true;
+                        result.Message = "Reservations were found";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Could not read reservations.", ex);
+                }
+            }
+            result.Data = result.Data.OrderBy(reservation => reservation.StartDate).ToList();
+
+            return;
         }
 
+        public void Add(Result<Reservation> reservation, string hostID)
+        {
+            Result<List<Reservation>> all = new Result<List<Reservation>>();
+            all.Data = new List<Reservation>();
+
+            GetReservationsByHostID(hostID, all);
+            //Don't check for success, as an empty list is acceptable
+
+            all.Data.Add(reservation.Data);
+            WriteToFile(all.Data, hostID);
+
+            reservation.Success = true;
+            reservation.Message = $"Reservation {reservation.Data.ID} added.";
+            return;
+        }
         public void Remove(Result<Reservation> reservation, string hostID)
         {
-            throw new NotImplementedException();
+            Result<List<Reservation>> all = new Result<List<Reservation>>();
+            all.Data = new List<Reservation>();
+
+            GetReservationsByHostID(hostID, all);
+            if (all.Success == false)
+            {
+                reservation.Success = false;
+                return;
+            }
+
+            if (all.Data.Where(r => r.Equals(reservation.Data)).ToList().Count == 0)
+            {
+                reservation.Success = false;
+                reservation.Message = $"Reservation ID {reservation.Data.ID} was not found. Exiting...";
+                return;
+            }
+            else
+            {
+                all.Data.Remove(reservation.Data);
+                WriteToFile(all.Data, hostID);
+
+                reservation.Success = true;
+                reservation.Message = $"Reservation {reservation.Data.ID} successfully deleted.";
+            }
+
+            return;
+        }
+        public void Edit(Result<Reservation> updatedReservation, string hostID)
+        {
+            Result<List<Reservation>> all = new Result<List<Reservation>>();
+            all.Data = new List<Reservation>();
+
+            GetReservationsByHostID(hostID, all);
+            if (all.Success == false)
+            {
+                updatedReservation.Success = false;
+                return;
+            }
+            int targetIndex = all.Data.IndexOf(all.Data.Where(r => r.ID == updatedReservation.Data.ID).FirstOrDefault());
+
+            if (targetIndex == -1)
+            {
+                updatedReservation.Success = false;
+                updatedReservation.Message = $"Reservation {updatedReservation.Data.ID} not found.";
+                return;
+            }
+            else
+            {
+                all.Data[targetIndex].StartDate = updatedReservation.Data.StartDate;
+                all.Data[targetIndex].EndDate = updatedReservation.Data.EndDate;
+                all.Data[targetIndex].Total = updatedReservation.Data.Total;
+                updatedReservation.Success = true;
+                updatedReservation.Message = $"Reservation {updatedReservation.Data.ID} updated.";
+                WriteToFile(all.Data, hostID);
+            }
+            return;
+        }
+
+
+        //File IO Functions
+        public Reservation Deserialize(string data)
+        {
+            Reservation result = new Reservation();
+            string[] fields = data.Split(",");
+            result.ID = int.Parse(fields[0]);
+            result.StartDate = DateTime.Parse(fields[1]);
+            result.EndDate = DateTime.Parse(fields[2]);
+            result.GuestID = int.Parse(fields[3]);
+            result.Total = decimal.Parse(fields[4]);
+
+            return result;
+        }
+        public string Serialize(Reservation reservation)
+        {
+            return $"{reservation.ID},{reservation.StartDate:yyyy-MM-dd},{reservation.EndDate:yyyy-MM-dd},{reservation.GuestID},{reservation.Total}";
+        }
+
+        public string GetFilePath(string hostID)
+        {
+            return Path.Combine(Test_Path, $"{hostID}.csv");
+        }
+        public void WriteToFile(List<Reservation> reservations, string hostID)
+        {
+            reservations = reservations.OrderBy(reservation => reservation.ID).ToList();
+            string filePath = GetFilePath(hostID);
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    //referenced stack overflow
+                    using (FileStream fs = File.Create(filePath)) { }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Could not create new file", ex);
+            }
+            try
+            {
+                using StreamWriter streamWriter = new StreamWriter(filePath);
+                streamWriter.WriteLine(HEADER);
+
+                if (reservations == null)
+                {
+                    return;
+                }
+                foreach (Reservation reservation in reservations)
+                {
+                    streamWriter.WriteLine(Serialize(reservation));
+                }
+            }
+            catch (IOException ex)
+            {
+                throw new Exception("Could not write reservations to file", ex);
+            }
         }
     }
 }
